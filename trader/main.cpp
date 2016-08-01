3199ce3fca25b3cc397cc4523f556ab2aa2de9fe
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-08-01 01:07:04 -0700

    add curlcpp dependency

--- trader/main.cpp
+++ trader/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "curl_easy.h"
+#include "curl_exception.h"
+
+using curl::curl_easy;
+using curl::curl_easy_exception;
+using curl::curlcpp_traceback;
 
 int main() {
-    std::cout << "getting hella bucks" << std::endl;
+    curl_easy easy;
+    easy.add<CURLOPT_URL>("http://www.google.com");
+    try {
+        easy.perform();
+    } catch (curl_easy_exception error) {
+        curlcpp_traceback errors = error.get_traceback();
+        error.print_traceback();
+    }
     return 0;
 }
